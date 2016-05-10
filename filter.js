3e86be9bc642ce8b0d42345bfe91524bdcab8f75
HCl Atomic <hcl@oceanvirtuel.com>
2016-05-10 17:21:44 +0200

    useless here

--- filter.js
+++
@@ -1,8 +0,0 @@
-$scp.$filter.numFilter = function(num) {
-	num = parseFloat(num);
-	return num.toFixed(2);
-};
-$scp.$filter.euroToDollar = function(num) {
-	num = parseFloat(num*1.2);
-	return num.toFixed(2);
-};
